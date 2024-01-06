5ee11d83c55ffc43bb56c23c51105cdefb18b855
이승규 <unhappydogchew@gmail.com>
2024-01-06 17:38:51 +0900

    Neogit 추가

--- init.lua
+++ init.lua
@@ -158,6 +158,18 @@ local plugins = {
   },
   {
     "s1n7ax/nvim-terminal"
+  },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua",              -- optional
+    },
+    config = true
   }
 }
 
@@ -196,10 +208,10 @@ vim.cmd.colorscheme "catppuccin-mocha"
 
 -- telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
 
 -- LuaSnip
 require("luasnip.loaders.from_vscode").lazy_load();
