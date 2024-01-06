a6301b5dcee80b12167e81e0ed8fa702d5a50db5
이승규 <unhappydogchew@gmail.com>
2024-01-06 16:57:23 +0900

    nvim-terminal 설정

--- init.lua
+++ init.lua
@@ -155,6 +155,9 @@ local plugins = {
     -- install jsregexp (optional!).
     build = "make install_jsregexp",
     dependencies = { "rafamadriz/friendly-snippets" },
+  },
+  {
+    "s1n7ax/nvim-terminal"
   }
 }
 
@@ -201,15 +204,19 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 -- LuaSnip
 require("luasnip.loaders.from_vscode").lazy_load();
 local ls = require("luasnip")
-vim.keymap.set({"i"}, "<Tab>", function() ls.expand() end, {silent = true})
-vim.keymap.set({"i", "s"}, "<Tab>", function() ls.jump( 1) end, {silent = true})
-vim.keymap.set({"i", "s"}, "<S-Tab>", function() ls.jump(-1) end, {silent = true})
+vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-H>", function() ls.jump(-1) end, {silent = true})
 
 vim.keymap.set({"i", "s"}, "<C-E>", function()
 	if ls.choice_active() then
 		ls.change_choice(1)
 	end
 end, {silent = true})
+
+-- nvim-terminal
+vim.o.hidden = true
+require('nvim-terminal').setup()
 
 -- options
 local opt = vim.opt
