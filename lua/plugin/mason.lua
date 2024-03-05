22731f9ebf54195865ce00c468f67c5bdffd1cf2
wingyou <winwing.you@gmail.com>
2024-03-05 11:08:30 +0900

    restore to original commit

--- lua/plugin/mason.lua
+++ lua/plugin/mason.lua
@@ -6,7 +6,7 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     opts = {
-        ensure_installed = { "tsserver", "jdtls", "rust_analyzer", "html", "cssls" },
+        ensure_installed = { "tsserver", "jdtls", "rust_analyzer", "html", "cssls", "ruby_ls" },
         automatic_installation = false,
         handlers = nil,
     },
